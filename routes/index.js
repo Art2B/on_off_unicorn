549fb3fe0b257c5000084ea08dce8a972f9c58db
Arthur Battut <arthur.battut@gmail.com>
2015-11-20 19:50:10 +0100

    update routes to more efficience

--- routes/index.js
+++ routes/index.js
@@ -9,29 +9,19 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'On Off Unicorn' });
 });
 
-router.post('/on', function(req, res){
-  var pythonFile = 'on.py';
-  PythonShell.run(pythonFile, function (err) {
-    if (err) throw err;
-    console.log('finished on');
-  });
-  res.send('Light on !');
-});
-router.post('/off', function(req, res){
-  var pythonFile = 'off.py';
+router.post('/:action', function(req, res){
+  var pythonFile = req.params.action+'.py';
   var pyshell = new PythonShell('off.py', {
     mode: 'text',
     scriptPath: '/'+global.appDir+'/python'
   });
-  res.send('Light off !');
-});
-router.post('/unicorn', function(req, res){
-  var pythonFile = 'unicorn.py';
-  PythonShell.run(pythonFile, function (err) {
-    if (err) throw err;
-    console.log('finished unicorn');
+  pyshell.end(function (err) {
+    if (err) {
+      res.status(300).send('Error');
+      throw err;
+    }
+    res.status(200).send('Ok');
   });
-  res.send('I\'m a fucking unicorn !');
 });
 
 module.exports = router;
